0a2e397ed5b208b922feffa2b2683a72dd29b292
Goncalo-Faria <gonca2372@gmail.com>
2019-08-16 04:46:22 +0100

    dd

--- architectures/cap_mlp_shared.py
+++ architectures/cap_mlp_shared.py
@@ -12,7 +12,7 @@ def setup(
         hparams):
     router = RNNRouting(
         metric=Frobenius(),
-        iterations=3,
+        iterations=5,
         cell = tf.compat.v1.nn.rnn_cell.LSTMCell(
             num_units=hparams.degree,
             name="attentionLayer",
