80de74ca4f4ba65cbe1b121a673fcb8f4756d18c
Goncalo-Faria <gonca2372@gmail.com>
2019-08-18 18:46:36 +0100

    dd

--- models/core/routing.py
+++ models/core/routing.py
@@ -200,7 +200,8 @@ class SimplifiedRoutingProcedure(RoutingProcedure):
 
             activations = tf.reshape(activations, shape=[-1] + activations.shape.as_list()[1:] + [1, 1])
 
-            c = self._normalization(r, axis=4)
+            #c = self._normalization(r, axis=4)
+            c=r
             ## c { batch, output_atoms, new_w , new_h, depth * np.prod(ksizes) }
 
             poses = self._renormalizedDotProd(c, votes)
@@ -223,7 +224,8 @@ class SimplifiedRoutingProcedure(RoutingProcedure):
                 r, s = self.compatibility(s, r, votes, poses, None, activations, it)
                 ## r :: { batch, output_atoms, new_w , new_h, depth * np.prod(ksizes) }
 
-                c = self._normalization(r, axis=4)
+                #c = self._normalization(r, axis=4)
+                c=r
                 ## c :: { batch, output_atoms, new_w , new_h, depth * np.prod(ksizes) }
 
                 poses = self._renormalizedDotProd(c, votes)
@@ -282,8 +284,8 @@ class HyperSimplifiedRoutingProcedure(RoutingProcedure):
             r, s = self.compatibility(None, None, votes, None, None, activations, None)
             ## r :: { batch, output_atoms, new_w , new_h, depth * np.prod(ksizes) }
 
-            #c = self._normalization(r, axis=4)
-            c = r
+            c = self._normalization(r, axis=4)
+            #c = r
             ## c :: { batch, output_atoms, new_w , new_h, depth * np.prod(ksizes) }
 
             poses = self._renormalizedDotProd(c, votes)
