81e47330cb25473d743498c43128ff76cf51c6b2
Goncalo-Faria <gonca2372@gmail.com>
2019-08-18 23:41:07 +0100

    dd

--- architectures/cap_kernel.py
+++ architectures/cap_kernel.py
@@ -15,7 +15,7 @@ def setup(
     router1 = KernelRouting(
                 kernel = MonoKernelMix(
                     kernel=SpectralMixture(hparams.verbose),
-                    degree=2),
+                    degree=1),
                 metric=Frobenius(),
                 iterations=3,
                 verbose=hparams.verbose,
