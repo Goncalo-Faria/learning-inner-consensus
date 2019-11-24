7b9e7dd44619edfda4196e38667616aa6d14cc85
Goncalo-Faria <gonca2372@gmail.com>
2019-11-24 14:10:58 +0000

    data

---
+++ tf_log/train/colect/download_f.py
@@ -0,0 +1,22 @@
+import wandb
+from shutil import copyfile
+
+api = wandb.Api()
+run = api.run("graf/Gulbenkian/thxtgsrj")
+start  = True
+meta  = None
+for file in run.files():
+    fname = file.name
+    fnames = fname.split(".")
+    print(fname)
+    if fnames[0] == "model":
+        if start :
+            if fnames[-1] == "meta":
+                meta = ".".join(fnames)
+                start = False
+        else:
+            copyfile(meta, ".".join(fnames[:-1]) + ".meta")
+            #print(meta)
+            #print(".".join(fnames[:-1]) + ".meta")
+
+        file.download(replace=True)
