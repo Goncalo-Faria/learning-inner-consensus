63cdf4003ebe1ddd59138cceb49a86e232cde607
goncalorafaria <goncalorafaria@tecnico.ulisboa.pt>
2020-09-26 02:08:19 +0100

    rmv

--- tf_log/train/showmodel.py
+++
@@ -1,9 +0,0 @@
-
-import tensorflow as tf
-from tensorflow.python.tools.inspect_checkpoint import print_tensors_in_checkpoint_file
-
-latest_ckp = tf.compat.v1.train.checkpoint_exists('KernelNet/model.ckpt-14208')
-
-print(latest_ckp)
-
-print_tensors_in_checkpoint_file('KernelNet/model.ckpt-14208', all_tensors=True, tensor_name='')
